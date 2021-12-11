8820bff4d6afd109a3073d43f89ae489d2e5e4db
ArtMan-8 <ya.Artriches@ya.ru>
2021-12-11 17:17:31 +0300

    feat: print weather

--- src/services/log.service.js
+++ src/services/log.service.js
@@ -18,3 +18,13 @@ export const printHelp = () => {
 		-t [API_KEY] - сохранить токен`)
 	);
 };
+
+export const printWeather = (res, icon) => {
+	console.log(
+		dedent(`${chalk.bgYellow(" WEATHER ")} Погода в городе ${res.name}
+		${icon}  ${res.weather[0].description}
+		Температура: ${res.main.temp} (ощущается ${res.main.feels_like})
+		Влажность: ${res.main.humidity} %
+		Скорость ветра: ${res.wind.speed} м/с`)
+	);
+};
--- src/weather.js
+++ src/weather.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+import {
+	getKeyValue,
+	saveKeyValue,
+	TOKEN_DICTIONARY,
+} from "./services/storage.service.js";
 import {
 	printerError,
 	printerSuccess,
 	printHelp,
+	printWeather,
 } from "./services/log.service.js";
-import { getWeather } from "./services/api.service.js";
+import { getIcon, getWeather } from "./services/api.service.js";
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -23,22 +28,52 @@ const saveToken = async (token) => {
 	}
 };
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printerError("Город не передан");
+		return;
+	}
+
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printerSuccess("Город сохранён");
+	} catch (e) {
+		printerError(e.message);
+	}
+};
+
+const getForcast = async () => {
+	try {
+		const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
+	} catch (e) {
+		if (e?.response?.status === 404) {
+			printerError("Неверно указан город");
+		} else if (e?.response?.status === 401) {
+			printerError("Неверно указан токен");
+		} else {
+			printerError(e.message);
+		}
+	}
+};
+
 const initCLI = () => {
 	const args = getArgs(process.argv);
 
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 
 	if (args.s) {
-		// save city
+		return saveCity(args.s);
 	}
 
 	if (args.t) {
 		return saveToken(args.t);
 	}
 
-	getWeather("moscow");
+	return getForcast();
 };
 
 initCLI();
