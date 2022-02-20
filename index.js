bc5ee39bbea95e14385b80512dae530fd4352875
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-20 17:36:59 +0300

    add: middleware example

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import { userRouter } from "./users/users.js";
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+	console.log("Time ", Date.now());
+	next();
+});
+
 app.get("/", (req, res) => {
 	res.append("Testing", "api");
 	res.type("application/json");
@@ -12,7 +17,16 @@ app.get("/", (req, res) => {
 	res.json("Hello");
 });
 
+app.get("/error", () => {
+	throw new Error("Error !!!");
+});
+
 app.use("/users", userRouter);
+
+app.use((error, req, res, next) => {
+	console.log(error.message);
+	res.status(500).send(error.message);
+});
 
 app.listen(port, () => {
 	console.log(`Server started on http://localhost:${port}`);
