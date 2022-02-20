69e2537bb99353df21ba6b18e843c4f5313d3ba6
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-20 17:04:00 +0300

    add: route samplee example

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
 
 const port = 8000;
-
 const app = express();
 
-app.get("/hello", (req, res) => {
+app.all("/hello", (req, res, next) => {
+	console.log("All");
+	next();
+});
+
+const cb = (req, res, next) => {
+	console.log("Cb");
+	next();
+};
+
+app.get("/hello", cb, (req, res) => {
 	res.send("Hello");
 });
+
+app.route("/user")
+	.get("/hello", (req, res) => {
+		res.send("GET Hello user");
+	})
+	.post("/hello", (req, res) => {
+		res.send("POST Hello user");
+	});
 
 app.listen(port, () => {
 	console.log(`Server started on http://localhost:${port}`);
