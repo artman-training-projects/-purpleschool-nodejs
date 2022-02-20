34f66997cbabbdf2aace4847ac70961335afcb93
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-20 17:16:04 +0300

    add: simple response example

--- index.js
+++ index.js
@@ -3,27 +3,12 @@ import express from "express";
 const port = 8000;
 const app = express();
 
-app.all("/hello", (req, res, next) => {
-	console.log("All");
-	next();
+app.get("/", (req, res) => {
+	res.append("Testing", "api");
+	res.type("application/json");
+	res.cookie("token", "dfjkhkgd");
+	res.json("Hello");
 });
-
-const cb = (req, res, next) => {
-	console.log("Cb");
-	next();
-};
-
-app.get("/hello", cb, (req, res) => {
-	res.send("Hello");
-});
-
-app.route("/user")
-	.get("/hello", (req, res) => {
-		res.send("GET Hello user");
-	})
-	.post("/hello", (req, res) => {
-		res.send("POST Hello user");
-	});
 
 app.listen(port, () => {
 	console.log(`Server started on http://localhost:${port}`);
