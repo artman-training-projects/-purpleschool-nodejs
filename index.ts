9239b4490358c4c6794622393cf57f72b96c7582
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-26 10:17:29 +0300

    init: TS

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import { userRouter } from "./users/users.js";
 
@@ -23,7 +23,7 @@ app.get("/error", () => {
 
 app.use("/users", userRouter);
 
-app.use((error, req, res, next) => {
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(error.message);
 	res.status(500).send(error.message);
 });
