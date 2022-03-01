da1d3405411648d298c00dd2c9a4fc44b4fcdd8c
ArtMan-8 <ya.Artriches@ya.ru>
2022-03-01 19:39:36 +0300

    add: linter & prettier

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import { Server } from "http";
-import express, { Express } from "express";
-import { injectable, inject } from "inversify";
-import "reflect-metadata";
+import { Server } from 'http';
+import express, { Express } from 'express';
+import { injectable, inject } from 'inversify';
+import 'reflect-metadata';
 
-import { TYPES } from "./types";
-import { UserController } from "./users/users.controller";
-import { ExeptionFilter } from "./errors/exeption.filter";
-import { ILogger } from "./logger/logger.interface";
+import { TYPES } from './types';
+import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
 
 @injectable()
 export class App {
@@ -17,21 +17,21 @@ export class App {
 	constructor(
 		@inject(TYPES.ILogger) private logger: ILogger,
 		@inject(TYPES.UserController) private userController: UserController,
-		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
 	) {
 		this.app = express();
 		this.port = 8000;
 	}
 
-	useRoutes() {
-		this.app.use("/users", this.userController.router);
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
 	}
 
-	useExeptionFilters() {
+	useExeptionFilters(): void {
 		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
 	}
 
-	public async init() {
+	public async init(): Promise<void> {
 		this.useRoutes();
 		this.useExeptionFilters();
 		this.server = this.app.listen(this.port);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,29 @@
-import { NextFunction, Request, Response } from "express";
-import { injectable, inject } from "inversify";
-import "reflect-metadata";
+import { NextFunction, Request, Response } from 'express';
+import { injectable, inject } from 'inversify';
+import 'reflect-metadata';
 
-import { TYPES } from "../types";
-import { BaseController } from "../common/base.controller";
-import { HTTPError } from "../errors/http-error.class";
-import { ILogger } from "../logger/logger.interface";
+import { TYPES } from '../types';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { ILogger } from '../logger/logger.interface';
 
-import { IUserController } from "./user.controller.interface";
+import { IUserController } from './user.controller.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
 		super(loggerService);
 		this.bindRoutes([
-			{ path: "/login", method: "post", func: this.login },
-			{ path: "/register", method: "post", func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+			{ path: '/register', method: 'post', func: this.register },
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction) {
-		next(new HTTPError(401, "Ошибка авторизации", "login"));
+	login(req: Request, res: Response, next: NextFunction): void {
+		next(new HTTPError(401, 'Ошибка авторизации', 'login'));
 	}
 
-	register(req: Request, res: Response, next: NextFunction) {
-		this.ok(res, "register");
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
 	}
 }
