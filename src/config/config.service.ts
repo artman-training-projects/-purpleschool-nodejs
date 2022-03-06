1198c02cf608e3ba4b393ae570792f5bcdcf1b9e
ArtMan-8 <ya.Artriches@ya.ru>
2022-03-06 17:31:31 +0300

    add: config Service

---
+++ src/config/config.service.ts
@@ -0,0 +1,27 @@
+import { injectable, inject } from 'inversify';
+import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
+
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+
+import { IConfigService } from './config.service.interface';
+
+@injectable()
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+		const result: DotenvConfigOutput = config();
+
+		if (result.error) {
+			this.logger.error('[ConfigService] Не удалось прочитать .env');
+		} else {
+			this.logger.log('[ConfigService] Конфигурация .env загружена');
+			this.config = result.parsed as DotenvParseOutput;
+		}
+	}
+
+	get(key: string): string {
+		return this.config[key];
+	}
+}
