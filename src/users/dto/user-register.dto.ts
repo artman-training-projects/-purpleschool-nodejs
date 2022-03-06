6ea9de8103694648a0e7ec30fe057ad069dd7935
ArtMan-8 <ya.Artriches@ya.ru>
2022-03-06 15:28:08 +0300

    add: user Validator

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsString({ message: 'Не указано имя' })
 	name: string;
+
+	@IsEmail({}, { message: 'Неверный формат email' })
 	email: string;
+
+	@IsString({ message: 'Не указан пароль' })
 	password: string;
 }
