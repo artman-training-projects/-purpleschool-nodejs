8a8cd97f43874af82b34be7654e7eee7b0e46777
ArtMan-8 <ya.Artriches@ya.ru>
2022-03-06 17:55:22 +0300

    add: db Prisma

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
 export const TYPES = {
 	Application: Symbol.for('Application'),
 	ILogger: Symbol.for('ILogger'),
+	ExeptionFilter: Symbol.for('ExeptionFilter'),
 	UserController: Symbol.for('UserController'),
 	UserService: Symbol.for('UserService'),
 	ConfigService: Symbol.for('ConfigService'),
-	ExeptionFilter: Symbol.for('ExeptionFilter'),
+	PrismaService: Symbol.for('PrismaService'),
 };
